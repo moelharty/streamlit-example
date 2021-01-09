c5409b4fbc7c2c057ccd88f7589a7d2a7f9786d1
moelharty <71794670+moelharty@users.noreply.github.com>
2021-01-09 23:05:37 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,38 +1,103 @@
-from collections import namedtuple
-import altair as alt
-import math
-import pandas as pd
 import streamlit as st
 
+import altair as alt
+from vega_datasets import data
+
+
+import pandas as pd
+import numpy as np
+
+# Streamlit
+# Page: https://www.streamlit.io/
+# Repo: https://github.com/streamlit/streamlit
+# Author: Adrian Treuille (https://www.streamlit.io/about)
+#   * Prof @ Carnegie Mellon University 
+#   * Google X
+#   * ZOOX (Self Driving Cars)
+#   * Streamlit
+
+df = pd.read_csv('df.csv')
+
+st.title('world happiness')
+st.markdown(
 """
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
+In this demo, we are going to analyze world happiness report data. You can filter the dataset in the sidebar.  
+Selected Cars:
 """
+)
+
+# Sidebar Controls
+st.sidebar.header('Filter Data:')
+year = st.sidebar.slider('Year', 2015, 2019, (2015,2019))
+origin = st.sidebar.multiselect('Continent', ['Europe', 'North America', 'Oceania','Asia','South America','Africa'], ['Europe', 'North America', 'Oceania','Asia','South America','Africa'])
+
+# Filter data by sidebar inputs:
+df = df[(df['Year'].dt.year.between(year[0],year[1])) & (df['Origin'].isin(origin))]
+df
+
+# Summary of selected data
+chart = alt.Chart(cars).mark_bar().encode(
+    x='count()',
+    y='Origin',
+    color='Origin'
+).properties(
+    width=300,
+    height=200
+) | alt.Chart(cars).mark_bar().encode(
+    alt.X("year(Year):N"),
+    y='count()',
+    color='Origin'
+).properties(
+    width=300,
+    height=200
+)
+chart
+
+st.markdown('## Projection of cars')
+
+projcars = df.dropna().reset_index(drop=True)
+
+features = st.multiselect('Features to project:', ["Freedom", "Generosity",'Happiness Rank','Happiness Score','Socia Support','Life Expectancy','Year'], ["Freedom", "Generosity",'Happiness Rank','Happiness Score','Socia Support','Life Expectancy','Year'])
+method_name = st.selectbox('Projection method:', ('PCA', 'MDS', 'TSNE'))
+
+projData = projcars.drop(projcars.columns.difference(features), axis=1)
+
+projData
+
+from sklearn import manifold
+from sklearn import decomposition
 
 
-with st.echo(code_location='below'):
-    total_points = st.slider("Number of points in spiral", 1, 5000, 2000)
-    num_turns = st.slider("Number of turns in spiral", 1, 100, 9)
+if method_name == 'PCA':
+  method = decomposition.PCA(n_components=2)
 
-    Point = namedtuple('Point', 'x y')
-    data = []
+if method_name == 'MDS':
+  method = manifold.MDS(n_components=2)
 
-    points_per_turn = total_points / num_turns
+if method_name == 'TSNE':
+  perplexity = st.slider('Perplexity', 10, 100, 30)
+  method = manifold.TSNE(n_components=2, perplexity=perplexity)
 
-    for curr_point_num in range(total_points):
-        curr_turn, i = divmod(curr_point_num, points_per_turn)
-        angle = (curr_turn + 1) * 2 * math.pi * i / points_per_turn
-        radius = curr_point_num / total_points
-        x = radius * math.cos(angle)
-        y = radius * math.sin(angle)
-        data.append(Point(x, y))
+  
 
-    st.altair_chart(alt.Chart(pd.DataFrame(data), height=500, width=500)
-        .mark_circle(color='#0068c9', opacity=0.5)
-        .encode(x='x:Q', y='y:Q'))
+placeholder = st.empty()
+placeholder.text('calculating...')
+  
+
+pos = pd.DataFrame(method.fit_transform(projData), columns=['x','y'])
+projcars = pd.concat([projcars, pos.reset_index(drop=True)], axis='columns')
+
+placeholder.empty()
+color = st.selectbox('Color by:', ("Freedom", "Generosity",'Happiness Rank','Happiness Score','Socia Support','Life Expectancy','Year'))
+# We use a point as mark
+chart = alt.Chart(projcars).mark_point().encode(
+    x='x',
+    y='y',
+    color=color
+).properties(
+    width=600,
+    height=600
+)
+chart
+
+projcars
