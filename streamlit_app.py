e359e29bf798330a99562bd35bc586051d8bbd3a
moelharty <71794670+moelharty@users.noreply.github.com>
2021-01-10 01:59:57 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -15,7 +15,7 @@ import numpy as np
 #   * Streamlit
 
 df = pd.read_csv('df.csv')
-df['Year'] = pd.to_datetime(df['Year'],format='%Y-%m-%d')
+df['Year'] = pd.to_datetime(df['Year'],format='%Y')
 #df['Year'] = pd.to_datetime(df['Year'],format='%Y')
 print(df)
 st.title('world happiness')
