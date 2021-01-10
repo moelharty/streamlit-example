86d6a1983c8b1a437292ecea23a66b5729eb8121
moelharty <71794670+moelharty@users.noreply.github.com>
2021-01-10 12:36:46 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -31,15 +31,15 @@ print(data)
 
 # Summary of selected data
 chart = alt.Chart(data).mark_bar().encode(
-    x='count()',
-    y='Continent',
-    color='Continent'
+    x='Continent',
+    y='Happiness Rank',
+    color='Year'
 ).properties(
     width=300,
     height=200
 ) | alt.Chart(data).mark_bar().encode(
     x='Continent',
-    y='Generosity',
+    y='Freedom',
     color='Year'
 ).properties(
     width=300,
